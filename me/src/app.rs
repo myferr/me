dff61c637cfec4ea2564166e91d6e512b521a9ae
myferr <contactme.myfer@protonmail.com>
2025-09-16 23:28:08 -0500

    feat: add header with links

--- me/src/app.rs
+++ me/src/app.rs
@@ -4,9 +4,16 @@ use yew::prelude::*;
 pub fn app() -> Html {
     html! {
         <main>
-            <img class="logo" src="https://yew.rs/img/logo.svg" alt="Yew logo" />
-            <h1>{ "Hello World!" }</h1>
-            <span class="subtitle">{ "from Yew" }</span>
+            <div class="flex items-center justify-between">
+                    <div class="flex items-center gap-2">
+                        <img class="w-10 h-10" src="https://github.com/myferr.png" alt="github.com/myferr" />
+                        <a class="font-bold text-lg text-white" href="/">{"myfer"}</a>
+                    </div>
+                    <div class="flex gap-2">
+                        <a class="text-white underline" href="https://github.com/myferr">{"github"}</a>
+                        <a class="text-white underline" href="https://x.com/myferdoescoding">{"x dot com"}</a>
+                    </div>
+            </div>
         </main>
     }
 }
