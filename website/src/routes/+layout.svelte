60437606b7512e616e54defee318cd15986260c6
myferr <contactme.myfer@protonmail.com>
2026-02-17 22:08:24 -0600

    add layout

--- website/src/routes/+layout.svelte
+++ website/src/routes/+layout.svelte
@@ -6,4 +6,16 @@
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
-{@render children()}
+
+<div class="bg-[#202120] text-white min-h-screen justify-center items-center flex flex-col">
+	<div>
+		{@render children()}
+	</div>
+</div>
+
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap');
+	* {
+		font-family: "Roboto", system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+	}
+</style>
